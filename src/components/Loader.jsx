74de7b3d9dcbb1342fca52f450eb74ae2c2859f0
Paul David Tan <cryxuz@gmail.com>
2023-11-25 19:53:27 +1300

    edited home

---
+++ src/components/Loader.jsx
@@ -0,0 +1,10 @@
+
+const Loader = () => {
+  return (
+    <div className="flex justift-center items-center">
+      <div className="w-20 h-20 border-2 border-opacity-20 border-blue-500 border-t-blue-500 rounded-full animate-spin" />
+    </div>
+  )
+}
+
+export default Loader
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,13 @@
+import {Canvas} from '@react-three/fiber'
+
+{/* <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
+        POPUP
+</div> */}
+
 const Home = () => {
   return (
     <section className="w-full h-screen relative">
-      <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
-        POPUP
-      </div>
+      
     </section>
   )
 }
