9a65adcb5cee5b6bfdeb5c59d2982a82508121b3
Paul David Tan <cryxuz@gmail.com>
2023-11-27 16:36:28 +1300

    finished HomeInfo component popups

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,11 +8,7 @@ import Bird from '../models/Bird'
 import Bluebird from '../models/Bluebird'
 import Balloon from '../models/Balloon'
 import Plane from '../models/Plane'
-
-
-{/* <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
-        POPUP
-</div> */}
+import HomeInfo from '../components/HomeInfo'
 
 const Home = () => {
   const [currentStage, setCurrentStage] = useState(1);
@@ -51,6 +47,9 @@ const Home = () => {
   const [islandScale, islandPosition, islandRotation] = adjustIslandForScreenSize()
   return (
     <section className="w-full h-screen relative">
+      <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
+        {currentStage && <HomeInfo currentStage={currentStage}/> }
+      </div>
       <Canvas 
         className={`w-full h-screen bg-transparent ${isRotating ? 'cursor-grabbing' : 'cursor-grab'}`} 
         camera={{ near: 0.1, far:1000 }}>
